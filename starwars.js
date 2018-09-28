66bbbada4178747301f978846068a74eef7d6bf8
Ana Luiza Sanches <analuiza@42codelab.com>
2018-09-28 01:25:33 -0300

    sort by episode

--- starwars.js
+++ starwars.js
@@ -3,25 +3,26 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
-
 $.ajax({
 	url: 'https://swapi.co/api/films/',
 	dataType: 'json',
 	success: response => {
 		console.log(response);
-		response.results.forEach(movie => {
+		let sortedMovies = response.results.sort(byEpisode);
+		sortedMovies.forEach(movie => {
 			add(movie);
 		});
 	}
 });
-
+function byEpisode(p, q){
+	return p.episode_id - q.episode_id;
+}
 function add(movie){
 	$('#movies ul')
 		.append($('<li>Episode ' + movie.episode_id +': '+ movie.title +'</li>')
 			.attr('data-episode-url', movie.url)
 			.click(event => { changeIntro(event)}));
 }
-
 function changeIntro(event){
 	$.ajax({
 		url: event.currentTarget.getAttribute('data-episode-url'),
