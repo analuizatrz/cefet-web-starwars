417bab1cb59d5981b27e41864bfc46706325487c
Ana Luiza Sanches <analuiza@42codelab.com>
2018-09-28 00:15:17 -0300

    show movie's titles (exercicio 2)

--- starwars.js
+++ starwars.js
@@ -3,40 +3,19 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
-
-
-// let requisicao = new XMLHttpRequest();
-// requisicao.onreadystatechange = callbackSWFilms;
-// requisicao.responseType = 'json';
-// requisicao.open('GET', '/swapi.co/api/films/');
-// requisicao.send();
-
-// function callbackSWFilms() {
-//   if (requisicao.readyState === 4) {  // 4: DONE (resp. recebida)
-//     if (requisicao.status === 200) {  // 200: código Ok do HTTP
-//       // a resposta chegou e foi um arquivo
-//       // .json com um array de tweets:
-//       let novosTweets = requisicao.response.arrayComNovosTweets;
-//       novosTweets.forEach(colocaTweetNaPagina);
-//     } else {
-//       console.log('Erro ao carregar mais tweets. Código HTTP: '
-//         + requisicao.status);
-//     }
-//   }
-// }
-
 $.ajax({
 	url: 'https://swapi.co/api/films/',
 	dataType: 'json',
-	success: function(resposta) {
-
-	  // resposta.results.forEach(adiciona);
-	  console.log(resposta);
+	success: function(response) {
+		console.log(response);
+		response.results.forEach(movie => {
+			add(movie);
+		});
 	}
 });
 
-
-function adiciona(ep){
-	let $li = $('<div></div>').appendTo(document.body.nav.ul);
-	$li.innerHTML = ep.title;
+function add(movie){
+	$('#movies ul')
+		.append($('<li>Episode ' + movie.episode_id +': '+ movie.title +'</li>')
+		.data('data-episode-url' , movie.url));
 }
